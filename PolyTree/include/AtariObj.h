a49c9146a4b19f9fdbbc3ff0b34130ee194fe528
Matt Lacey <lacey@proximityinsight.com>
2020-07-16 22:50:36 +1000

    WIP - start of tree generation

--- PolyTree/include/AtariObj.h
+++ PolyTree/include/AtariObj.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <vector>
+
 extern "C"
 {
 	#include "Obj.h"
@@ -9,11 +11,15 @@ class AtariObj
 {
 public:
 	Obj o;
+	float* fpVerts;
 	
 	AtariObj(char* filename);
 	void Render();
+	void GenerateTree();
 
 private:
 	unsigned int VAO, VBO, EBO;
+	void GenerateNode(ObjNode* node, std::vector<ObjFace> faces);
 	void SetupBuffers();
+	bool FacesIntersect(ObjFace* face1, ObjFace* face2);
 };
--- PolyTree/src/AtariObj.cpp
+++ PolyTree/src/AtariObj.cpp
@@ -4,15 +4,23 @@
 #include <GLFW/glfw3.h>
 
 #include <stdlib.h>
+#include <vector>
+#include <algorithm>
 
 AtariObj::AtariObj(char* filename)
 {
 	o = loadObj(filename);
+    fpVerts = NULL;
     SetupBuffers();
 }
 
 void AtariObj::SetupBuffers()
 {
+    if (fpVerts)
+    {
+        return;
+    }
+
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
@@ -21,7 +29,7 @@ void AtariObj::SetupBuffers()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
     // Atari verts are in 16.16 fixed point format, so we need to convert them here to the correct format for OpenGL
-    float* fpVerts = (float*)malloc(sizeof(float) * 3 * o.vertCount);
+    fpVerts = (float*)malloc(sizeof(float) * 3 * o.vertCount);
 
     for (int i = 0; i < o.vertCount; i++)
     {
@@ -39,6 +47,60 @@ void AtariObj::SetupBuffers()
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
     glBindVertexArray(0);
+}
+
+void AtariObj::GenerateTree()
+{
+    o.pRootNode = (ObjNode*)malloc(sizeof(ObjNode));
+    std::vector<ObjFace> faces(o.faceCount);
+    std::random_shuffle(faces.begin(), faces.end());
+
+    GenerateNode(o.pRootNode, faces);
+}
+
+void AtariObj::GenerateNode(ObjNode* node, std::vector<ObjFace> faces)
+{
+    // if no we're down to one face (later, no intersecting faces) stop
+    if (faces.size() <= 1)
+    {
+        node->pPart = (ObjPart*)malloc(sizeof(ObjPart));
+        node->pPart->faces = &faces[0];
+        node->pPart->faceCount = (long)faces.size();
+        return;
+    }
+
+    std::vector<ObjFace> left;
+    std::vector<ObjFace> right;
+
+    // try out different plane options and find a reasonable split ratio (3:1)
+    long bestVert = -1;
+    float ratio = 999999.9f;
+
+    // for (int i = 0; i < 3; i++)
+    int i = 0;
+    {
+        long vert = ((long*)faces.data())[i];
+
+        // 0 for x, 1 for y, 2 for z - same type used in the ObjPlane struct
+        //for (int j = 0; j < 3; j++)
+        int j = 0;
+        {
+            float offset = fpVerts[vert * 3 + j];
+
+            // sift faces to left and right based on verts being greater
+            // or less than the offset
+
+            // if ratio is closer to 1 then update bestVert and best ratio
+        }
+    }
+
+    // once a reasonable split is found then alloc a new node for each size,
+    // wash, rinse, repeat?
+}
+
+bool AtariObj::FacesIntersect(ObjFace* face1, ObjFace* face2)
+{
+    return false;
 }
 
 void AtariObj::Render()
